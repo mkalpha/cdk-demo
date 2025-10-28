29d2e898a470a5b14054cc69842afcebedf7f73c
Andrew Workman <andrew.workman@gamma.co.uk>
2025-10-28 15:06:13 +0000

    Adds basic github actions to deploy CDK stack

--- bin/cdk-demo.ts
+++ bin/cdk-demo.ts
@@ -1,9 +1,9 @@
 #!/opt/homebrew/opt/node/bin/node
-import * as cdk from 'aws-cdk-lib';
-import { CdkDemoStack } from '../lib/cdk-demo-stack';
+import * as cdk from "aws-cdk-lib";
+import { CdkDemoStack } from "../lib/cdk-demo-stack";
 
 const app = new cdk.App();
-new CdkDemoStack(app, 'CdkDemoStack', {
+new CdkDemoStack(app, "CdkDemoStack", {
   /* If you don't specify 'env', this stack will be environment-agnostic.
    * Account/Region-dependent features and context lookups will not work,
    * but a single synthesized template can be deployed anywhere. */
@@ -14,7 +14,7 @@ new CdkDemoStack(app, 'CdkDemoStack', {
 
   /* Uncomment the next line if you know exactly what Account and Region you
    * want to deploy the stack to. */
-  // env: { account: '123456789012', region: 'us-east-1' },
+  env: { account: "098150072242", region: "eu-west-2" },
 
   /* For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html */
 });
